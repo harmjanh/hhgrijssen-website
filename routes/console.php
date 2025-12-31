b0e9e5cb58ef884b5532273e7cf3b93b28ad3efc
Harm-Jan Hazelhorst <h.hazelhorst@code14.nl>
2025-12-31 13:28:26 +0100

    disabled syncs

--- routes/console.php
+++ routes/console.php
@@ -5,24 +5,24 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('youtube:sync')
-    ->hourly()
-    ->between('06:00', '23:00')
-    ->withoutOverlapping()
-    ->runInBackground();
+// Schedule::command('youtube:sync')
+//     ->hourly()
+//     ->between('06:00', '23:00')
+//     ->withoutOverlapping()
+//     ->runInBackground();
 
 // hourly sync agenda items
-Schedule::command('import:ical-feeds')
-    ->hourlyAt(5)
-    ->between('06:00', '23:00')
-    ->withoutOverlapping()
-    ->runInBackground();
+// Schedule::command('import:ical-feeds')
+//     ->hourlyAt(5)
+//     ->between('06:00', '23:00')
+//     ->withoutOverlapping()
+//     ->runInBackground();
 
-Schedule::command('app:create-services-command')
-    ->hourlyAt(15)
-    ->between('06:00', '23:00')
-    ->withoutOverlapping()
-    ->runInBackground();
+// Schedule::command('app:create-services-command')
+//     ->hourlyAt(15)
+//     ->between('06:00', '23:00')
+//     ->withoutOverlapping()
+//     ->runInBackground();
 
 // Schedule reservation reminders to run daily at 8 AM
 Schedule::command('reservations:send-reminders')
