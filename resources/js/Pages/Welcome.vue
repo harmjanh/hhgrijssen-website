30a131deee477a2023009578094073435bbdf7b0
Harm-Jan Hazelhorst <h.hazelhorst@code14.nl>
2026-01-03 20:49:33 +0100

    header home less height

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -58,7 +58,7 @@ function handleImageError() {
         <!--    &lt;!&ndash;<div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />&ndash;&gt;-->
         <!--</div>-->
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
                 <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                     <!--<div class="relative rounded-full px-3 py-1 text-sm/6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">-->
                     <!--    Announcing our next round of funding. <a href="#" class="font-semibold text-white"><span class="absolute inset-0" aria-hidden="true" />Read more <span aria-hidden="true">&rarr;</span></a>-->
